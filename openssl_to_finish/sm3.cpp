50f2645b621166264ba285df36eeff05f673690f
shuosureS <2426191562@qq.com>
2022-07-20 17:36:18 +0800

    sm3.cpp

--- openssl_to_finish/sm3.cpp
+++ openssl_to_finish/sm3.cpp
@@ -1,6 +1,7 @@
 #include"openssl/evp.h"
 #include"openssl/rsa.h"
 #include<iostream>
+//time=792875ms
 using namespace std;
 int sm3_openssl(const void* message, size_t len, unsigned char* hash)
 {
@@ -16,21 +17,61 @@ int sm3_openssl(const void* message, size_t len, unsigned char* hash)
     return 0;
 }
 
+int inttohex(int aa, unsigned char* buffer)
+{
+    static int  i = 0;
+    i = 0;
+    if (aa < 16)            
+    {
+        if (aa < 10)        
+            buffer[i] = aa + '0';
+        else
+            buffer[i] = aa - 10 + 'a';
+        buffer[i + 1] = '\0'; 
+    }
+    else
+    {
+        inttohex(aa / 16, buffer);  
+        i++;                
+        aa %= 16;           
+        if (aa < 10)        
+            buffer[i] = aa + '0';
+        else
+            buffer[i] = aa - 10 + 'a';
+    }
+
+
+    return i+1;
+}
+
 int main()
 {
-    unsigned char input2[256] = "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd";
-    int ilen2 = 64;
-    unsigned char output2[32];
-    int i2;
-    // ctx;
-
+    unsigned char tinput[256] = "abcddef123456908213456a";
+    int tlen = 23;
+    unsigned char tagart[32];
+    unsigned char input[256] = "";
+    sm3_openssl(tinput, tlen, tagart);
+    int ilen = 0;
+    unsigned char output[32];
+    int i=0;
     int start, end;
     start = clock();
-    for (int i = 0; i < 10000000; i++) {
-        sm3_openssl(input2, ilen2, output2);
+    while (1) {
+        ilen=inttohex(i, input);        
+        sm3_openssl(input, ilen, output);
+        
+        if (output[0]==tagart[0] &&output[1]==tagart[1]&&output[2]==tagart[2] && output[3] == tagart[3])
+        {
+            end = clock();
+            printf("¹¥»÷³É¹¦\n");
+            printf("time=%dms\n", end - start);
+            
+            break;
+        }
+        i++;
     }
-    end = clock();
-    printf("time=%dms\n", end - start);
+    
+   
 
-    system("pause");
+    
 }
